e484e84f12f2b84b7be21022cbff94324bc90c0f
oxxun21 <ogaeun0121@gmail.com>
2025-10-01 22:24:32 +0900

    initial commit

---
+++ hello_world_new/lib/main.dart
@@ -0,0 +1,18 @@
+// 모든 코드 블럭을 포함하고 있어서 항상 import
+import 'package:flutter/material.dart';
+
+// main.dart가 최상위 파일
+void main(){
+  // 패키지로 부터 불러와서 사용
+  // runApp:플러터 앱을 실행
+  runApp(
+    // 여기에 화면이 들어감
+    // MaterialApp(디자인)은 항상 최상위에 위치
+    // runApp에 들어가는 첫번째 값은 무조건 MaterialApp
+    // MaterialApp 바로 아래에는 Scaffold(기본구조 생성) 고정
+    MaterialApp(
+      home: Scaffold(),
+    ),
+  );
+}
+
